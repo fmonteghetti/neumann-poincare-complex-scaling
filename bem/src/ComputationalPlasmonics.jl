165146c691ee896cca19f3b512270cc497de286b
Florian Monteghetti <f.monteghetti@gmail.com>
2026-03-24 19:13:18 +0100

    Remove dependency on Makie.jl to speed up compilation.

--- bem/src/ComputationalPlasmonics.jl
+++ bem/src/ComputationalPlasmonics.jl
@@ -3,13 +3,11 @@ module ComputationalPlasmonics
 const PROJECT_ROOT = pkgdir(ComputationalPlasmonics)
 
 using StaticArrays
-using CairoMakie
 using LinearAlgebra
 using Inti
 
 include("pml.jl")
 include("operators.jl")
-include("makietheme.jl")
 
 export CartesianPML, EulerPML, single_layer, adjoint_double_layer, solution, solution_full
 
